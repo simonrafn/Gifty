07afbcc2b775d2ba247b47bbd3b56df8616bddd0
Arnar Barri Símonarson <arnarb89@gmail.com>
2018-01-22 14:37:02 +0000

    text to char

--- mylist/models.py
+++ mylist/models.py
@@ -5,7 +5,7 @@ from index import models as index_models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    link = models.TextField(max_length=1000, null=True)
+    link = models.CharField(max_length=1000, null=True)
     price = models.CharField(max_length=20)
     owner_id = models.ForeignKey(index_models.User)
     is_removed = models.BooleanField(default=False)
--- notifications/models.py
+++ notifications/models.py
@@ -6,6 +6,6 @@ from index import models as index_models
 class Notification(models.Model):
     user_id = models.ForeignKey(index_models.User)
     is_seen = models.BooleanField(default=False)
-    link = models.TextField(max_length=1000, null=True)
-    message = models.TextField(max_length=1000)
-    date = models.DateField()
+    link = models.CharField(max_length=1000, null=True)
+    message = models.CharField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
