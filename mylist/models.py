3c0df5f5d228128e1a096dae9514f941fbf3d9f7
Arnar Barri Símonarson <arnarb89@gmail.com>
2018-01-22 14:50:45 +0000

    on_delete cascade

--- mylist/models.py
+++ mylist/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     link = models.CharField(max_length=1000, null=True)
     price = models.CharField(max_length=20)
-    owner_id = models.ForeignKey(index_models.User)
+    owner_id = models.ForeignKey(index_models.User, on_delete=models.CASCADE)
     is_removed = models.BooleanField(default=False)
-    reserver_id = models.ForeignKey(index_models.User, null=True)
+    reserver_id = models.ForeignKey(index_models.User, null=True, on_delete=models.CASCADE)
 
--- notifications/models.py
+++ notifications/models.py
@@ -4,7 +4,7 @@ from index import models as index_models
 
 # Create your models here.
 class Notification(models.Model):
-    user_id = models.ForeignKey(index_models.User)
+    user_id = models.ForeignKey(index_models.User, on_delete=models.CASCADE)
     is_seen = models.BooleanField(default=False)
     link = models.CharField(max_length=1000, null=True)
     message = models.CharField(max_length=1000)
