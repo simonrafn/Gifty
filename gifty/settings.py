4ab788255a48e2261088b0e103c18f69d00a766c
simonrafn <simonbjarnason@gmail.com>
2018-01-22 14:14:58 +0000

    Added models for user, contacts

--- gifty/settings.py
+++ gifty/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'index',
+    'user',
+    'contacts',
+    'notifications',
+    'mylist',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=200, unique=True)
+    avatar = models.CharField(max_length=10) # Temporary, replace later with something that can store images
+
+
+class Comment(models.Model):
+    item_id = models.ForeignKey('mylist.Item')
+    commenter_id = models.ForeignKey('index.User')
+    message = models.CharField(max_length=5000)
+    date = models.DateTimeField()
+    visible_to_owner = models.BooleanField()
+
